612404e61ead5d4f1320235c50ac18e9e4933e83
Marisa You <marisa.you@gmail.com>
2020-10-14 20:31:01 -0700

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,9 @@
 require 'pry'
 
 class Artist
-  extend Memorable 
+  extend Memorable::ClassMethods
   extend Findable
+  include Memorable::InstanceMethods
   include Paramable
 
   attr_accessor :name
@@ -11,7 +12,7 @@ class Artist
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,15 @@
 require 'pry'
 
 class Song
-  extend Memorable
+  extend Memorable::ClassMethods
   extend Findable
+  include Memorable::InstanceMethods
   include Paramable
 
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
 
   def self.all
     @@songs
